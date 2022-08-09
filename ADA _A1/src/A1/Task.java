ceecd945080caa5b70364c836afa4554b0df47e0
sheetalskumar <sheetal.s.kumar@outlook.com>
2022-08-09 16:20:50 +1200

    1 thread working in Task

--- ADA _A1/src/A1/Task.java
+++ ADA _A1/src/A1/Task.java
@@ -8,6 +8,47 @@ package A1;
  *
  * @author Sheet
  */
-public class Task {
-    
+public class Task implements Runnable
+{
+   private int x;
+   private int getId;
+   
+   public Task()
+   {   
+       x = 0;
+       int id = getId; 
+   }
+   
+   // repeatedly increment the value of x 
+   public void run()
+   {  
+      System.out.println("Thread " + Thread.currentThread()
+         + " started with x = " + x);
+      int loopIterations = (x + 1) * 10;
+      for (int i = 0; i < loopIterations; i++)
+      {  
+          x++; 
+          x++; 
+      }
+      System.out.println("Thread " + Thread.currentThread()
+         + " finishing with x = " + x); 
+   }
+   
+   public static void main(String[] args)
+   {  // create two concurrent threads
+      Task task = new Task();
+      Thread threadA = new Thread(task);
+      //Thread threadB = new Thread(task);
+      threadA.start();
+      //threadB.start();
+      //try
+      //{  // wait for both threads to finish
+         //threadA.join();
+         //threadB.join();
+      //}
+      //catch (InterruptedException e)
+      //{  System.out.println("Interrupted " + e);
+      //}
+      System.out.println("The final value of x is " + task.x);
+   }
 }
