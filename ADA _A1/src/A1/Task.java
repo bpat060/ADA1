7942d58f09f71b20b04c3e72876df145355260b4
Bhakti Patel <87521790+bpat060@users.noreply.github.com>
2022-08-09 15:54:42 +1200

    vd

--- ADA _A1/src/A1/Task.java
+++ ADA _A1/src/A1/Task.java
@@ -5,71 +5,10 @@
  */
 package A1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
 /**
  *
  * @author Owner
  */
-// inner class that represents a single game played across a socket
-class Task implements Runnable {
+public class Task {
 
-    private int min, max; // minimum (incl) and maximum (excl) range
-
-    private int value; // value to guess
-    private Socket socket; // socket for client/server communication
-
-    // constructor for a guess game to guess value across a socket
-    // for client/server communication
-    public Task(Socket socket, int value) {
-        this.value = value;
-        this.socket = socket;
-    }
-
-    public void run() {
-        PrintWriter pw; // output stream to client
-        BufferedReader br; // input stream from client
-        try {  // create an autoflush output stream for the socket
-            pw = new PrintWriter(socket.getOutputStream(), true);
-            // create a buffered input stream for this socket
-            br = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
-            // play the game until value is correctly guessed
-            pw.println("Guess the number between " + min + " and "
-                    + (max - 1) + " inclusive");
-            int guess = min - 1;
-            do {
-                String clientGuess = br.readLine();
-                String response;
-                if (clientGuess == null) {
-                    response = "Nothing entered, try again";
-                } else {
-                    try {
-                        guess = Integer.parseInt(clientGuess);
-                        if (guess < value) {
-                            response = "Guess too low, try again";
-                        } else if (guess > value) {
-                            response = "Guess too high, try again";
-                        } else {
-                            response = "Correct guess!";
-                        }
-                    } catch (NumberFormatException e) {
-                        response = "Not an int value, try again";
-                    }
-                }
-                pw.println(response);
-            } while (guess != value);
-            pw.close();
-            br.close();
-            System.out.println("Closing connection with "
-                    + socket.getInetAddress());
-            socket.close();
-        } catch (IOException e) {
-            System.err.println("Server error with game: " + e);
-        }
-    }
 }
--- ADA _A1/src/A1/taskObserver.java
+++ ADA _A1/src/A1/taskObserver.java
@@ -8,7 +8,10 @@ package A1;
  *
  * @author Andrew Ensor
  */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -49,7 +52,7 @@ public class TaskObserver {
                         + socket.getInetAddress());
                 // start a game with this connection, note that a server
                 // might typically keep a reference to each game
-                Task game = new Task(socket,
+                GuessGame game = new GuessGame(socket,
                         generator.nextInt(max - min) + min);
                 Thread thread = new Thread(game);
                 thread.start();
@@ -71,5 +74,62 @@ public class TaskObserver {
     public static void main(String[] args) {
         TaskObserver server = new TaskObserver(1, 100);
         server.startServer();
+    }
+
+    // inner class that represents a single game played across a socket
+    private class GuessGame implements Runnable {
+
+        private int value; // value to guess
+        private Socket socket; // socket for client/server communication
+
+        // constructor for a guess game to guess value across a socket
+        // for client/server communication
+        public GuessGame(Socket socket, int value) {
+            this.value = value;
+            this.socket = socket;
+        }
+
+        public void run() {
+            PrintWriter pw; // output stream to client
+            BufferedReader br; // input stream from client
+            try {  // create an autoflush output stream for the socket
+                pw = new PrintWriter(socket.getOutputStream(), true);
+                // create a buffered input stream for this socket
+                br = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream()));
+                // play the game until value is correctly guessed
+                pw.println("Guess the number between " + min + " and "
+                        + (max - 1) + " inclusive");
+                int guess = min - 1;
+                do {
+                    String clientGuess = br.readLine();
+                    String response;
+                    if (clientGuess == null) {
+                        response = "Nothing entered, try again";
+                    } else {
+                        try {
+                            guess = Integer.parseInt(clientGuess);
+                            if (guess < value) {
+                                response = "Guess too low, try again";
+                            } else if (guess > value) {
+                                response = "Guess too high, try again";
+                            } else {
+                                response = "Correct guess!";
+                            }
+                        } catch (NumberFormatException e) {
+                            response = "Not an int value, try again";
+                        }
+                    }
+                    pw.println(response);
+                } while (guess != value);
+                pw.close();
+                br.close();
+                System.out.println("Closing connection with "
+                        + socket.getInetAddress());
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Server error with game: " + e);
+            }
+        }
     }
 }
