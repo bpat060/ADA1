2b62acf56f6e98c60210c0c345bd94c7c6a61335
Bhakti Patel <87521790+bpat060@users.noreply.github.com>
2022-08-12 14:16:22 +1200

    hk,

--- ADA _A1/src/A1/B/ThreadPool.java
+++ ADA _A1/src/A1/B/ThreadPool.java
@@ -12,26 +12,26 @@ import java.util.List;
  *
  * @author Owner
  */
-public class ThreadPool {
+public abstract class ThreadPool implements Runnable {
 
-    Task E;
-    List<Task> pool = new ArrayList<Task>();
+    Runnable runnable;
+    List<Runnable> pool = new ArrayList<Runnable>();
     int initialSize;
 
     public ThreadPool(int initialSize) {
         this.initialSize = initialSize;
-        this.E = E;
+        this.runnable = runnable;
         this.pool = pool;
     }
 
     public int getSize() {
-        return 0; //dunno if get size is relative to initial size++?
+        return pool.size(); //dunno if get size is relative to initial size++?
     }
 
     public int getAvailable() { // get available threads?
         //Thread.currentThread().isAlive();
         int num;
-        if (perform(E) == true) {
+        if (perform(runnable) == true) {
             num = 1;
         } else {
             num = 0;
@@ -40,15 +40,52 @@ public class ThreadPool {
     }
 
     public void resize(int newSize) {
-
+        //if pool reaches max num threads
+        //make pool list longer by a resonable amount for new size
     }
 
     public void destroyPool() {
-
+        //all tasks executed in queue before destroying threadpool worker threads
     }
 
     public boolean perform(Runnable task) {
 
         return false;
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("From primary() method: " + Thread.currentThread().getName());
+
+        System.out.println("Implementing the runnable interface in Java");
+
+        Runnable instance = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("From run() method: " + Thread.currentThread().getName());
+
+            }
+
+        };
+
+        System.out.println("Create a new instance of the thread object.");
+
+        Thread test1 = new Thread(instance);
+        System.out.println("Executing the thread!");
+        test1.start();
+
+        Thread test2 = new Thread(instance);
+        System.out.println("Executing the thread!");
+        test2.start();
+
+        Thread test3 = new Thread(instance);
+        System.out.println("Executing the thread!");
+        test3.start();
+
+        Thread test4 = new Thread(instance);
+        System.out.println("Executing the thread!");
+        test4.start();
+
+    }
+
 }
